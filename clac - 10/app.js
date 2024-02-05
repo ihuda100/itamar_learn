cda8a3f6664c7ea89e6333831294c968589d5f20
ihuda100 <ihuda100@gmail.com>
2024-02-05 18:52:10 +0200

    finish EX-1

--- clac - 10/app.js
+++ clac - 10/app.js
@@ -6,6 +6,8 @@ let num1 = document.querySelector('#num1');
 let num2 = document.querySelector('#num2');
 let multy = document.querySelector('#multy');
 let divied = document.querySelector('#divied');
+let plus = document.querySelector('#plus');
+let minus = document.querySelector('#minus');
 let res = document.querySelector('#res');
 
 let calc=()=>{
@@ -15,6 +17,12 @@ let calc=()=>{
     divied.addEventListener('click',()=>{
         res.innerHTML = num1.value / num2.value;
     });
+    plus.addEventListener('click',()=>{
+        res.innerHTML = num1.value + num2.value;
+    })
+    minus.addEventListener('click',()=>{
+        res.innerHTML = num1.value - num2.value;
+    })
 }
 
 init();
